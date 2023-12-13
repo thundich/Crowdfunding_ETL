f27d192005d12c2d4dec83cf82871e2b91007ffe
thundich <tamaradurgan@gmail.com>
2023-12-12 21:31:07 -0500

    Commiting to GitHub

---
+++ Project 2 Queries.sql
@@ -0,0 +1,9 @@
+select * from public."Subcategory";
+
+select * from public."Campaign";
+
+select * from public."Category";
+
+select * from public."Contacts";
+
+select * from public."Crowdfunding"
